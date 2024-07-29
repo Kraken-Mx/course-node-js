3988f203b55421ec37cb8fb22fe023dfe5147552
Kraken_Mx <aldo.gustavo.mr@gmail.com>
2024-07-28 23:20:45 -0600

    CommonJS modules

---
+++ cjs.js
@@ -0,0 +1,27 @@
+/*
+ *
+ * CommonJS modules
+ * https://nodejs.org/api/modules.html
+ * 
+*/
+
+const a = 10
+const b = 10
+
+// CommonJS require module
+const { sum } = require('./cjs/sum.js')
+const { global_object } = require('./cjs/global_object.js')
+const circle = require('./cjs/circle.js')
+
+console.log(`La suma es: ${sum(a, b)}.`)
+
+console.log(`The area of a circle of radius 4 is: ${circle.area(4)}`);
+console.log(`The circumference of a circle of radius 4 is: ${circle.circumference(4)}`);
+
+
+/*
+console.log(`--> El objeto global de Javascript es globalThis: `)
+console.log(global_object())
+console.log(`---AQUI TERMINA GLOBAL---`)
+*/
+
