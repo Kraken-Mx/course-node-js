09f7b39a112ed0374e19fa0ecce076d739157dd0
Kraken_Mx <aldo.gustavo.mr@gmail.com>
2024-08-01 22:55:02 -0600

    uso del método fs.readdir()

---
+++ 11.ls.js
@@ -0,0 +1,24 @@
+/*
+ *
+ * 'fs.readdir()' method:
+ * Is used to asynchronously read the contents of a given directory.
+ * https://www.educative.io/answers/what-is-the-stream-fsreaddir-method-in-nodejs
+ * 
+*/
+const fs = require('node:fs'); 
+
+fs.readdir('.', { withFileTypes: true }, (err, files) => {
+    console.log('Reading data from directory...')
+    if (err)
+        console.log('Error!' + err);
+    else {
+        //console.log(files)
+        files.forEach(file => {
+            if(file.isDirectory())
+                console.log(file.name + ' --> DIRECTORY')
+            else
+            console.log(file.name + ' --> OTHER')
+        })
+    }
+})
+
