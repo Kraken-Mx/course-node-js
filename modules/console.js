bbc4b53671cb9fd78bc6b4851f644a14400db331
Kraken_Mx <aldo.gustavo.mr@gmail.com>
2024-08-11 15:34:52 -0600

    se añade descripción

--- modules/console.js
+++ modules/console.js
@@ -1,14 +1,17 @@
 /*
  *
- * Use console
+ * Use Console Object
+ * Instance methods of the console object.
+ * https://developer.mozilla.org/en-US/docs/Web/API/console
  * 
 */
 
-function console (a) {
+function consola (a) {
     console.log(`Hola ${a}, bienvenido a node !`)
     console.info(`Hola ${a}, bienvenido a node !`)
     console.error(`Hola ${a}, bienvenido a node !`)
 
     return a
 }
+consola('Aldo')
 
